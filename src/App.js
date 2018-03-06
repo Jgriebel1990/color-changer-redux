0934af98ed576761ce2f11ef449561dd83391f86
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-05 20:59:43 -0500

    created my react app and installed redux and react-redux and added a div

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <div>
+       ô1111
+       <input type="color" name="" id=""/>
+     </div>
+    );
+  }
+}
+
+export default App;
